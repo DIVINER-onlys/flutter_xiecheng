e7204da6342c0a90c7e1bdd2a78bddf8ac3fd983
linhaoran <1476589247@qq.com>
2019-06-12 20:40:55 +0800

    feat: 首页loading组件以及上拉刷新功能

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -7,10 +7,13 @@ import 'package:flutter_xiecheng/model/home_model.dart' show HomeModel;
 import 'package:flutter_xiecheng/model/common_model.dart' show CommonModel;
 import 'package:flutter_xiecheng/model/grid_nav_model.dart' show GridNavModel;
 import 'package:flutter_xiecheng/model/sales_box_model.dart' show SalesBoxModel;
+import 'package:flutter_xiecheng/widget/webview.dart' show WebView;
 import 'package:flutter_xiecheng/widget/grid_nav.dart' show GridNav;
 import 'package:flutter_xiecheng/widget/local_nav.dart' show LocalNav;
 import 'package:flutter_xiecheng/widget/sub_nav.dart' show SubNav;
 import 'package:flutter_xiecheng/widget/sales_box.dart' show SalesBox;
+import 'package:flutter_xiecheng/widget/loading_container.dart' show LoadingContainer;
+
 const APPBAR_SCROLL_OFFSET = 100;
 
 class HomePage extends StatefulWidget {
@@ -25,18 +28,20 @@ class _HomePageState extends State<HomePage> {
     'http://210.76.68.128/zxwcms//gd_zxw/upload/file/201805/30121611jwtf.jpg'
   ];
   double appBarAlpha = 0;
+  List<CommonModel> bannerList = [];
   List<CommonModel> localNavList = [];
   GridNavModel gridNavModel;
   List<CommonModel> subNavList = [];
   SalesBoxModel salesBox;
+  bool _loading = true;
 
   @override
   void initState() {
     super.initState();
-    loadData();
+    _handleRefesh();
   }
 
-  loadData() async {
+  Future<Null> _handleRefesh() async {
     // HomeDao.fetch().then((result) {
     //   setState((){
     //     resultString = json.encode(result);
@@ -50,14 +55,20 @@ class _HomePageState extends State<HomePage> {
     try {
       HomeModel model = await HomeDao.fetch();
       setState((){
+        bannerList = model.bannerList;
         localNavList = model.localNavList;
         gridNavModel = model.griNav;
         subNavList = model.subNavList;
         salesBox = model.salesBox;
+        _loading = false;
       });
     } catch(e) {
+      setState(() {
+        _loading = false;
+      });
       print(e);
     }
+    return null;
   }
 
   _onScroll(offset) {
@@ -78,11 +89,14 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xfff2f2f2),
-      body: Stack(
-        children: <Widget>[
-          mainListView(),
-          mainAppBar()
-        ],
+      body: LoadingContainer(
+        isLoading: _loading,
+        child: Stack(
+          children: <Widget>[
+            mainListView(),
+            mainAppBar()
+          ],
+        ),
       )
     );
   }
@@ -91,48 +105,75 @@ class _HomePageState extends State<HomePage> {
     return MediaQuery.removePadding(
       removeTop: true,
       context: context,
-      child: NotificationListener(
-        onNotification: (scrollNotification) {
-          if (scrollNotification is ScrollUpdateNotification && scrollNotification.depth == 0) {
-            // 滚动且是列表滚动的时候
-            _onScroll(scrollNotification.metrics.pixels);
-          }
-        },
-        child: ListView(
-          children: <Widget>[
-            Container(
-              height: 160.0,
-              child: Swiper(
-                itemCount: _imageUris.length,
-                autoplay: true,
-                itemBuilder: (BuildContext context, int index) {
-                  return Image.network(
-                    _imageUris[index],
-                    fit: BoxFit.fill,
-                  );
-                },
-                pagination: SwiperPagination(),
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.fromLTRB(7, 4, 7, 4),
-              child: LocalNav(localNavList: localNavList),
-            ),
-            Padding(
-              padding: EdgeInsets.fromLTRB(7, 0, 7, 4),
-              child: GridNav(gridNavModel: gridNavModel)
-            ),
-            Padding(
-              padding: EdgeInsets.fromLTRB(7, 0, 7, 4),
-              child: SubNav(subNavList: subNavList),
-            ),
-            Padding(
-              padding: EdgeInsets.fromLTRB(7, 0, 7, 4),
-              child: SalesBox(salesBox: salesBox),
-            )
-          ],
+      child: RefreshIndicator( // 上拉刷新组件
+        onRefresh: _handleRefesh,
+        child: NotificationListener(
+          onNotification: (scrollNotification) {
+            if (scrollNotification is ScrollUpdateNotification && scrollNotification.depth == 0) {
+              // 滚动且是列表滚动的时候
+              _onScroll(scrollNotification.metrics.pixels);
+            }
+          },
+          child: _listView
         ),
       )
+    );
+  }
+
+  Widget get _listView {
+    return ListView(
+      children: <Widget>[
+        _banner,
+        Padding(
+          padding: EdgeInsets.fromLTRB(7, 4, 7, 4),
+          child: LocalNav(localNavList: localNavList),
+        ),
+        Padding(
+          padding: EdgeInsets.fromLTRB(7, 0, 7, 4),
+          child: GridNav(gridNavModel: gridNavModel)
+        ),
+        Padding(
+          padding: EdgeInsets.fromLTRB(7, 0, 7, 4),
+          child: SubNav(subNavList: subNavList),
+        ),
+        Padding(
+          padding: EdgeInsets.fromLTRB(7, 0, 7, 4),
+          child: SalesBox(salesBox: salesBox),
+        )
+      ],
+    );
+  }
+
+  Widget get _banner {
+    return Container(
+      height: 160.0,
+      child: Swiper(
+        itemCount: bannerList.length,
+        autoplay: true,
+        itemBuilder: (BuildContext context, int index) {
+          return GestureDetector(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) {
+                    return WebView(
+                      url: bannerList[index].url,
+                      title: bannerList[index].title,
+                      hideAppBar: bannerList[index].hideAppBar,
+                    );
+                  }
+                )
+              );
+            },
+            child: Image.network(
+              bannerList[index].icon,
+              fit: BoxFit.fill,
+            ),
+          );
+        },
+        pagination: SwiperPagination(),
+      ),
     );
   }
 
